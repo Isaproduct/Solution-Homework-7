9055f91d12d0df56db866fccf92546f7ad4945c1
Isaproduct <islam.ilimbayev@narxoz.kz>
2025-04-26 18:07:30 +0500

    i change the execurorservice

--- part2/AirportSimulation.java
+++ part2/AirportSimulation.java
@@ -10,12 +10,17 @@ public class AirportSimulation {
                 aircraftList.add(new PassengerPlane("A1", 100));
                 aircraftList.add(new CargoPlane("C1", 200));
                 aircraftList.add(new Helicopter("H1", 50));
+                Map<Aircraft, Boolean> hasSentRequest = new HashMap<>();
 
                 ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
                 executorService.scheduleAtFixedRate(() -> {
+
                         for (Aircraft aircraft : aircraftList) {
-                                if (Math.random() > 0.5) {
-                                        aircraft.send("Request Runway", tower);
+                                if (!hasSentRequest.getOrDefault(aircraft, false)) {
+                                        if (Math.random() > 0.5) {
+                                                aircraft.send("Request Runway", tower);
+                                                hasSentRequest.put(aircraft, true);
+                                        }
                                 }
                         }
                         tower.manageRunway();
